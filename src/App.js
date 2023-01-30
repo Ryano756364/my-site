0ec6cf0e57fe40bfe6706f9a66864c2b3d14b7f4
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-01-29 22:12:53 -0500

    Landing page begun + components started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import ProjectItem from './components/ProjectItem';
+
 function App() {
   return (
     <div>
-      <h2>My Site</h2>
+      <h2>Ryan O'Dell</h2>
+      <p>This is a site about me</p>
+      <ProjectItem /> 
     </div>
   );
 }
 
 export default App;
+ 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';  //importing ReactDOM from dependencies -> react + react-dom -> one giant library
 
-import './index.css';
-import App from './App';
+import './index.css';  //injects css code
+import App from './App';  //app.js file that is being imported
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App />);  //normally this wouldn't work but because it's transformed before it's delivered to the browser
