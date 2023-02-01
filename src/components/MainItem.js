cd62d895a092d5d520002c8f37ced9c4e3985bf7
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-01 18:33:39 -0500

    Project photo items added

--- src/components/MainItem.js
+++ src/components/MainItem.js
@@ -17,7 +17,7 @@ function MainItem(){
         {/* Project One */}
         <div className="main-column__item" id="home-buyer-container">
           <Parallax bgImage={require('../img/home-bg-large-2.jpg')} bgImageAlt="Three homes with blue sky background" strength={-200}>
-            <div className="main-column-text-container right-text-container">
+            <div className="main-column-text-container home-text-container">
               <div className="main-column__item-header">
                 Home Buyer Contact Form
               </div>
@@ -33,18 +33,19 @@ function MainItem(){
 
         {/* Project Two */}
         <div className="main-column__item">
-          <Parallax bgImageAlt="City view" strength={-200}>
-          <div className="main-column-text-container left-text-container">
+          <Parallax bgImage={require('../img/bg-travel-giant.jpg')} bgImageAlt="City view" strength={-200}>
+          <div className="main-column-text-container booking-text-container">
             <div className="main-column__item-header">
-              Item Header
+            Travel Booking Site Project
             </div>
 
             <div className="main-column__item-description">
-              Item Description
+              This project displays a knowledge of the flex design system along with creative elements to a nav bar and notifications attached to icons.
             </div>
 
             <div className="main-column__item-link-container">
-              <a href="https:www.google.com" className='item-link-container__link'>Link</a>
+              {/* <a href="https:www.google.com" className='item-link-container__link'>Link</a> */}
+              Project Link Coming Soon
             </div>
           </div>
           </Parallax>
@@ -52,19 +53,22 @@ function MainItem(){
 
         {/* Project Three */}
         <div className="main-column__item">
-          <div className="main-column-text-container right-text-container">
+          <Parallax bgImage={require('../img/bg-agent-giant.jpg')} bgImageAlt="Concrete home" strength={-200}>
+          <div className="main-column-text-container agent-text-container">
             <div className="main-column__item-header ">
-              Item Header
+              Real Estate Agent Profile Project
             </div>
 
             <div className="main-column__item-description">
-              Item Description
+              This project displays a high affinity to utilizing the two-dimensional grid system to display great control over the site layout.
             </div>
 
             <div className="main-column__item-link-container">
-              <a href="https:www.google.com" className='item-link-container__link'>Link</a>
+              {/* <a href="https:www.google.com" className='item-link-container__link'>Link</a> */}
+              Project Link Coming Soon
             </div>
           </div>
+          </Parallax>
         </div>
 
       </div>
