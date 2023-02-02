d03c5693a6a74890e57007bd226027b3d21a802e
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-01 22:24:35 -0500

    Website cleanup + reponsive design started

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,28 @@ import './Footer.css';
 
 function Footer(){
   return (
-    <div className='footer-container'>
-      Footer
+    <div className='footer'>
+      <div className='footer__logo-box'>
+        <img src={require("../img/logo-white.png")} alt="Ryan's Logo" className="logo"/>
+      </div>
+
+      <div className='row'>
+        <div className='col-1-of-2'>
+          <div className='footer__navigation'>
+            <ul className='footer__list'>
+              <li className='footer__item'><a href='https://www.google.com' className='footer-link'>About Me</a></li>
+              <li className='footer__item'><a href='https://www.google.com' className='footer-link'>More Projects</a></li>
+              <li className='footer__item'><a href='https://www.google.com' className='footer-link'>LinkedIn</a></li>
+              <li className='footer__item'><a href='https://www.google.com' className='footer-link'>Github</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='col-1-of-2'>
+          <p className='footer__ending'>
+            Built by Ryan O'Dell. Projects will continue to be uploaded on an ongoing basis. Check back soon to see what new applications are built! 
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
