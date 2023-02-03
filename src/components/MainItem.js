9faadd6bd29643899cfe3bc7ccf28fae1392b641
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-02 22:30:13 -0500

    Small cleanup

--- src/components/MainItem.js
+++ src/components/MainItem.js
@@ -19,7 +19,7 @@ function MainItem(){
           <Parallax bgImage={require('../img/home-bg-large-2.jpg')} bgImageAlt="Three homes with blue sky background" strength={-200}>
             <div className="main-column-text-container home-text-container">
               <div className="main-column__item-header">
-                Home Buyer Contact Form
+                Pegasus, LLC Contact Form
               </div>
               <div className="main-column__item-description">
                 This project displays knowledge of css-grid system to format a website with creative implementations added to menu, pricing, and contact form.
@@ -36,7 +36,7 @@ function MainItem(){
           <Parallax bgImage={require('../img/bg-travel-giant.jpg')} bgImageAlt="City view" strength={-200}>
           <div className="main-column-text-container booking-text-container">
             <div className="main-column__item-header">
-            Travel Booking Site Project
+            R-Pod Booking Site Project
             </div>
 
             <div className="main-column__item-description">
@@ -44,8 +44,7 @@ function MainItem(){
             </div>
 
             <div className="main-column__item-link-container">
-              {/* <a href="https:www.google.com" className='item-link-container__link'>Link</a> */}
-              Project Link Coming Soon
+              <a href="https://ryano756364.github.io/rpod-booking-site/" className='item-link-container__link'>Project Link</a>
             </div>
           </div>
           </Parallax>
