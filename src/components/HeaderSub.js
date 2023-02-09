dba5ebca04ca690394a6a746733ea792b79738f3
rodell <ryanodell2668@gmail.com>
2023-02-08 19:29:49 -0500

    Webm video added for mobile

--- src/components/HeaderSub.js
+++ src/components/HeaderSub.js
@@ -1,19 +1,14 @@
 import './HeaderSub.css';
-/* import VideoGiant from '../img/videos/background-video-giant.mp4' */
 import VideoGiant from '../img/videos/bg-vd-giant.mp4';
+import VideoGiantWebm from '../img/videos/bg-vd-giant.webm';
 
 function HeaderSub(){
   return (
     <div className="header-sub-container">
-      {/* <div className="video-container">
-        <video src={VideoGiant} autoPlay loop muted className="video-container__video">
-
-        </video>
-      </div> */}
-
       <div className='video-container'>
         <video className='video-container__content' autoPlay loop muted>
           <source src={VideoGiant} type='video/mp4' className="video-container__video"></source>
+          <source src={VideoGiantWebm} type='video/webm' className="video-container__video"></source>
           Background video is to play here!
         </video>
       </div>
